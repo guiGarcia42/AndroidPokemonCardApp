1e3a56b51efd3ceb387a16bd136da19ba51435b3
Guilherme Garcia <gui123.garcia@gmail.com>
2023-05-19 12:20:17 -0300

    Finishing ActivityResult feature

--- app/src/main/java/com/example/guilhermegarcia_rm87192/Constants.kt
+++ app/src/main/java/com/example/guilhermegarcia_rm87192/Constants.kt
@@ -1,13 +1,16 @@
 package com.example.guilhermegarcia_rm87192
 
-object Theme {
+object Constants {
+    const val ACTIVITY_SKILL = "Activity Skills"
+    const val ACTIVITY_MAIN = "Activity Main"
+
     var currentTheme = R.style.Theme_GuilhermeGarcia_RM87192
 
     private const val SCARLET = R.style.Theme_GuilhermeGarcia_RM87192
     private const val VIOLET = R.style.ThemeViolet
 
     fun switchTheme(){
-        Theme.currentTheme = when(Theme.currentTheme){
+        Constants.currentTheme = when(Constants.currentTheme){
             SCARLET -> VIOLET
             VIOLET -> SCARLET
             else -> -1
